f1cca2969c4cd54262753794c8cdea72b0bd06c1
mowaiscyphion <mowaiscyphion@gmail.com>
2024-02-19 18:18:50 +0500

    get student API integrated

---
+++ src/components/base/LoadingScreen.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import { Color } from '../../utils/color'
+import { screenDimensions } from '../../utils/functions'
+
+const LoadingScreen = ({ loading }) => {
+    return (
+        <>
+            {loading ? <View style={styles.main}>
+                <ActivityIndicator size={'large'} color={Color.primary} />
+            </View> : null}
+        </>
+    )
+}
+
+export default LoadingScreen
+
+const styles = StyleSheet.create({
+    main: {
+        position: 'absolute',
+        width: screenDimensions.width,
+        height: screenDimensions.height,
+        // backgroundColor: "rgba(0, 0, 0, 0.9)",
+        backgroundColor: Color.white,
+        zIndex: 10000,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
